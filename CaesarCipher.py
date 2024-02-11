34f465623c3ad8b9f8922166551ec1eccfbb1dd9
“goksualc” <goksualcinkaya@gmail.com>
2024-02-11 16:57:36 +0300

    encrypt

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -8,15 +8,34 @@ def caesar_encrypt(plain_text):
     plain_text = plain_text.upper()
 
     for c in plain_text:
-        c='E'
+      
 
         index = ALPHABET.find(c)
-
 
         index = (index + KEY) % len(ALPHABET)
 
         cipher_text = cipher_text + ALPHABET[index]
 
-
         return cipher_text 
 
+
+    def caesar_decrypt(cipher_text):     
+        plain_text = ''
+
+        for c in cipher_text:
+            index = ALPHABET.find(c)
+            index = (index - KEY) % len(ALPHABET)
+            plain_text = plain_text + ALPHABET[index]
+
+
+            return plain_text
+        
+
+if __name__ == '__main__':
+
+    m = 'Welcome to my github repo'
+
+encrypted = caesar_encrypt(m)
+print(encrypted)
+print(caesar_encrypt(encrypted)
+      )
