301ec34aeea742321fe6b0d26427e9cc4ab3ba99
goksualc <goksualcinkaya@gmail.com>
2024-02-12 10:15:31 +0300

    caesar latest

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,41 +1,30 @@
 ALPHABET = ' ABCDEFGHIJKLMNOPQRSTUVWYZ'
 KEY = 3
 
-
 def caesar_encrypt(plain_text):
-
     cipher_text = ''
     plain_text = plain_text.upper()
 
     for c in plain_text:
-      
-
         index = ALPHABET.find(c)
-
         index = (index + KEY) % len(ALPHABET)
-
         cipher_text = cipher_text + ALPHABET[index]
 
-        return cipher_text 
+    return cipher_text
 
+def caesar_decrypt(cipher_text):
+    plain_text = ''
 
-    def caesar_decrypt(cipher_text):     
-        plain_text = ''
+    for c in cipher_text:
+        index = ALPHABET.find(c)
+        index = (index - KEY) % len(ALPHABET)
+        plain_text = plain_text + ALPHABET[index]
 
-        for c in cipher_text:
-            index = ALPHABET.find(c)
-            index = (index - KEY) % len(ALPHABET)
-            plain_text = plain_text + ALPHABET[index]
-
-
-            return plain_text
-        
+    return plain_text
 
 if __name__ == '__main__':
-
     m = 'Welcome to my github repo'
-
-encrypted = caesar_encrypt(m)
-print(encrypted)
-print(caesar_encrypt(encrypted)
-      )
+    encrypted = caesar_encrypt(m)
+    print("Encrypted:", encrypted)
+    decrypted = caesar_decrypt(encrypted)
+    print("Decrypted:", decrypted)
